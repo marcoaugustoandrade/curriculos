6c34ff73fc9ba39e4af590bc0f0f8be54a6e74bc
Marco Andrade <marco.andrade@ifro.edu.br>
2021-03-15 19:47:22 -0400

    Aula 5 - detalhando e cadatrando novas instituições

--- app/controllers/instituicoes.py
+++ app/controllers/instituicoes.py
@@ -1,8 +1,31 @@
-from app import app
-from flask import render_template
+from app import app, db
+from flask import render_template, request
 from app.models.tables import Instituicao
 
 @app.route('/instituicoes')
 def listar_instituicoes():
     i = Instituicao.query.all()
     return render_template('listar_instituicoes.html', instituicao=i)
+
+@app.route('/instituicoes/<instituicao_id>')
+def detalhar_instituicoes(instituicao_id):
+    i = Instituicao.query.filter_by(id=instituicao_id).first()
+    return render_template('detalhar_instituicoes.html', i=i)
+
+@app.route('/instituicoes/nova')
+def carregar_formulario():
+    return render_template('formulario_instituicoes.html')
+
+@app.route('/instituicoes/inserir', methods=['POST'])
+def inserir_instituicao():
+    # Recebendo os dados do formulário
+    nome = request.form['nome']
+    sigla = request.form['sigla']
+    
+    # Cadastrando no banco de dados
+    i = Instituicao(nome=nome, sigla=sigla)
+    db.session.add(i)
+    db.session.commit()
+
+    instituicoes = Instituicao.query.all()
+    return render_template('listar_instituicoes.html', instituicao=instituicoes)
